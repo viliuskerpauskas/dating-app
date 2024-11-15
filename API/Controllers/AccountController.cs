590f75ba9bec68d2ea1e7f992846aab083891e9f
Vilius Kerpauskas <vilius.kerpauskas@softdent.lt>
2024-11-15 15:28:40 +0200

    40. Creating a User DTO and returning the token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,15 +3,16 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(DataContext context) : BaseApiController
+public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
 {
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
         if (await UserExists(registerDto.Username))
         {
@@ -28,11 +29,11 @@ public class AccountController(DataContext context) : BaseApiController
 
         context.Users.Add(user);
         await context.SaveChangesAsync();
-        return user;
+        return new UserDto { UserName = user.UserName, Token = tokenService.CreateToken(user) };
     }
 
     [HttpPost("login")]
-    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
         var user = await context.Users.FirstOrDefaultAsync(x =>
             x.UserName == loginDto.UserName.ToLower()
@@ -54,7 +55,7 @@ public class AccountController(DataContext context) : BaseApiController
             }
         }
 
-        return user;
+        return new UserDto { UserName = user.UserName, Token = tokenService.CreateToken(user) };
     }
 
     private async Task<bool> UserExists(string username)
